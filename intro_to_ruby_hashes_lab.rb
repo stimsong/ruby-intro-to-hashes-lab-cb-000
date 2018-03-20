f249f7ea08a4a72ac13067f388bd390e13bd91a8
Greg Stimson <stimsong@yahoo.com>
2018-03-20 18:16:00 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,9 +33,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :names => {
-        :reading_railroad => {
-          mortgage_value: "$100"
-        },
+        :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline => {}
